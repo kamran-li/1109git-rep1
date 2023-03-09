976390ddcf60378105a240c9368db69902b708a6
李阳 <2271378911@qq.com>
2023-03-09 14:49:35 +0800

    第一次

---
+++ hello/src/main/java/org/example/hello.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * ClassName: hello
+ * Package: org.example
+ * Description:
+ *
+ * @Author 李大仙儿
+ * @Create 2023/3/9 14:48
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸");
+    }
+}
